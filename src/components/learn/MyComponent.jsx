86f226368b8e4a744bcc4a55e8fe4a5a5f780d67
hoangtth <bibilove9876@gmail.com>
2024-07-28 00:47:21 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,20 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const hoidanit = "eric 1"; //string
+    // const hoidanit = 25; //number
+    // const hoidanit = true; //boolean
+    // const hoidanit = undefined; //undefined
+    // const hoidanit = null;
+    const hoidanit = [1, 2, 3];
+    // const hoidanit = {
+    //     name: "eric",
+    //     age: 25,
+    // }
     return (
         <>
-            <div className="">Hoang & hoi dan IT</div>
+            <div className="">{JSON.stringify(hoidanit)} & hoi dan IT</div>
+            <div>{console.log("Eric")}</div>
             <div className="child" style={{ borderRadius: "10px" }} >child</div>
         </>
     );
