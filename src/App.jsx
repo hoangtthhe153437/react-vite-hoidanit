e327ac565c9b1cd37dc80d92c7e62e1efe5cd723
hoangtth <bibilove9876@gmail.com>
2024-07-27 23:33:34 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// () => {}
+
+const MyComponent = () => {
+  return (
+    <div>
+      <p>Hoang & hoi dan IT</p>
+    </div>
+  );
+}
+
 const App = () => {
   const [count, setCount] = useState(0)
 
-  // function myFunction() {
-  //   console.log("Run my function")
-  // }
 
-  const myFunction = () => {
-    console.log("Run my arrow function")
-  }
-
-  myFunction();
 
   return (
     <>
@@ -27,6 +29,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello World</h1>
+      <MyComponent></MyComponent>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
