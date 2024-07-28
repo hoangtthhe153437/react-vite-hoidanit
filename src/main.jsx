9d8e3068ed8f664d7f608c43ac6ddc92d5ce68d8
hoangtth <bibilove9876@gmail.com>
2024-07-28 23:34:04 +0700

    video 52

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,16 @@ import {
 import LoginPage from './pages/login.jsx';
 import RegisterPage from './pages/register.jsx';
 import UserPage from './pages/user.jsx';
-import ProductPage from './pages/product.jsx';
+import BookPage from './pages/book.jsx';
 import './styles/global.css'
 import TodoApp from './components/todo/TodoApp.jsx';
+import ErrorPage from './pages/error.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -27,7 +29,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/products",
-        element: <ProductPage />
+        element: <BookPage />
       }
     ]
   },
---
+++ src/pages/book.jsx
@@ -0,0 +1,10 @@
+
+const BookPage = () => {
+    return (
+        <div>
+            <h1>Book Page</h1>
+        </div>
+    );
+}
+
+export default BookPage;
