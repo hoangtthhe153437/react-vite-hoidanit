1a27cf9633ce9a4e50bd0b898e01370eedad0c47
F88\hoangtth <hoangtth.work@gmail.com>
2024-07-29 16:13:53 +0700

    video 70

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,6 +1,6 @@
-import { Button, Input } from "antd";
+import { Button, Input, notification } from "antd";
 import { useState } from "react";
-import axios from "axios";
+import { createUserAPI } from "../../services/api.service";
 
 const UserForm = () => {
   const [fullName, setFullName] = useState("");
@@ -8,17 +8,17 @@ const UserForm = () => {
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
 
-const handleClickButton = () => {
-    const URL_BACKEND = "http://localhost:8080/api/v1/user";
-    const data = {
-        fullName : fullName,
-        email : email,
-        password : password,
-        phone : phone
+  const handleClickButton = async () => {
+    const res = await createUserAPI(fullName, email, password, phone);
+
+    if (res.data) {
+      notification.success({
+        message: "Create user",
+        description: "Tạo user thành công",
+      });
     }
-    axios.post(URL_BACKEND, data)
-    console.log(">>>> check state: " , {email, password, fullName, phone});
-}
+    console.log(">>>> check res: ", res.data);
+  };
 
   return (
     <div className="user-form">
@@ -63,8 +63,10 @@ const handleClickButton = () => {
           />
         </div>
         <div>
-          <Button onClick={handleClickButton} type="primary">Submit</Button>
-        </div> 
+          <Button onClick={handleClickButton} type="primary">
+            Submit
+          </Button>
+        </div>
       </div>
     </div>
   );
