d72257a3e4136de769251e28160541a97292d24f
hoangtth <bibilove9876@gmail.com>
2024-08-01 22:34:40 +0700

    video 98

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,41 +1,47 @@
 import { Link, NavLink } from "react-router-dom";
 import { Menu } from "antd";
-import {  AuditOutlined, HomeOutlined, UsergroupAddOutlined } from '@ant-design/icons';
-import { useState } from "react";
+import { AuditOutlined, HomeOutlined, UsergroupAddOutlined } from '@ant-design/icons';
+import { useContext, useState } from "react";
+import { AuthContext } from "../context/auth.context";
 
 const Header = () => {
-  const [current, setCurrent] = useState("");
-  const onClick = (e) => {
-    console.log("click ", e);
-    setCurrent(e.key);
-  };
+    const [current, setCurrent] = useState("");
 
-  const items = [
-    {
-      label: <Link to={"/"}>Home</Link>,
-      key: "home",
-      icon: <HomeOutlined />,
-    },
-    {
-      label: <Link to={"/users"}>Users</Link>,
-      key: "users",
-      icon: <UsergroupAddOutlined />,
-    },
-    {
-      label: <Link to={"/books"}>Books</Link>,
-      key: "books",
-      icon: <AuditOutlined />,
-    },
-  ];
+    const { user } = useContext(AuthContext);
 
-  return (
-    <Menu
-      onClick={onClick}
-      selectedKeys={[current]}
-      mode="horizontal"
-      items={items}
-    />
-  );
+    console.log("user ", user);
+
+    const onClick = (e) => {
+        console.log("click ", e);
+        setCurrent(e.key);
+    };
+
+    const items = [
+        {
+            label: <Link to={"/"}>Home</Link>,
+            key: "home",
+            icon: <HomeOutlined />,
+        },
+        {
+            label: <Link to={"/users"}>Users</Link>,
+            key: "users",
+            icon: <UsergroupAddOutlined />,
+        },
+        {
+            label: <Link to={"/books"}>Books</Link>,
+            key: "books",
+            icon: <AuditOutlined />,
+        },
+    ];
+
+    return (
+        <Menu
+            onClick={onClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+            items={items}
+        />
+    );
 };
 
 export default Header;
